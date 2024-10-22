7c1fd5dbbe8b94ce00991bb2a5ac624301fb2eec
Giorgio Martino <giorgio.martino.28@gmail.com>
2024-10-22 17:43:26 +0200

    update base vite.config

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import '@mantine/core/styles.css';
 
+import React from 'react';
 import { MantineProvider } from '@mantine/core';
+import Bubbles from '@/sketches/bubbles/bubbles';
 import { theme } from './theme';
-import React from "react";
-import Bubbles from "@/sketches/bubbles/bubbles";
 
 export default function App() {
   return (
     <MantineProvider theme={theme}>
-      <Bubbles/>
+      <Bubbles />
     </MantineProvider>
   );
 }
